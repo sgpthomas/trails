d04072422d3019764ad47814a3dee38279661aea
Aditi Kabra <aditi@dhcp-clara-687.eduroam.cornell.edu>
2016-10-02 20:57:38 -0400

    half a testing message

--- Trails/app/src/main/java/com/ksatgames/trails/MainActivity.java
+++ Trails/app/src/main/java/com/ksatgames/trails/MainActivity.java
@@ -22,24 +22,6 @@ public class MainActivity extends Activity {
         setContentView(gameView);
 
     }
-//max is maximum number of obstacles per line. assumption: less then half of width
-    int[][] genMatrix(int l, int w,int max) //generated original matrix; to be called before start of game
-    {
-        int matrix[][]=new int[l][w];
-       for(int i=0; i<l; i++) {
-           int[] a=new int[max];
-           for (int j = 0; j < max; j++) {
-               a[j] = (int) Math.random() * l;
-           }
-           int k=0;
-           for(int j=0; j<w; j++)
-               if(j==a[k])
-               {
-                   matrix[i][j]=a[k++];
-               }
-       }
-    return matrix;
-    }
     // This method executes when the player starts the game
     @Override
     protected void onResume() {
--- Trails/app/src/main/java/come/ksatgame/trails/gameEngine/Generator.java
+++ Trails/app/src/main/java/come/ksatgame/trails/gameEngine/Generator.java
@@ -5,4 +5,22 @@ package come.ksatgame.trails.gameEngine;
  */
 
 public class Generator {
+    //max is maximum number of obstacles per line. assumption: less then half of width
+    int[][] genMatrix(int l, int w,int max) //generated original matrix; to be called before start of game
+    {
+        int matrix[][]=new int[l][w];
+        for(int i=0; i<l; i++) {
+            int[] a=new int[max];
+            for (int j = 0; j < max; j++) {
+                a[j] = (int) Math.random() * l;
+            }
+            int k=0;
+            for(int j=0; j<w; j++)
+                if(j==a[k])
+                {
+                    matrix[i][j]=a[k++];
+                }
+        }
+        return matrix;
+    }
 }
