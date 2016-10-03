d47142a191dff649c6f54d4e28fe70995707456f
Aditi Kabra <aditi@dhcp-clara-687.eduroam.cornell.edu>
2016-10-02 23:43:49 -0400

    matrix generator fixed

--- Trails/app/src/main/java/come/ksatgame/trails/gameEngine/Generator.java
+++ Trails/app/src/main/java/come/ksatgame/trails/gameEngine/Generator.java
@@ -19,42 +19,48 @@ public class Generator {
 
     }
 
-
-    static private int[][] genMatrix(int l, int w, int max) //generated original matrix; to be called before start of game
+    public int[][] genMatrix(int l, int w, int max) //generated original matrix; to be called before start of game
     {
-        int track = (int) (Math.random() * l);    //keeps track of the usable track
+        int track= (int) (Math.random()*w);	//keeps track of the usable track
         int matrix[][] = new int[l][w];
-        for (int i = 1; i < l; i++) {
+        for (int i = 0; i < l; i++) {
             int[] a = new int[max];
-            for (int j = 0; j < max; ) {
-                int x = (int) (Math.random() * l);
-                if (x != track) {
-                    a[j] = x;
-                    j++;
-                }
+            for (int j = 0; j < max;) {
+                int x = (int) (Math.random()*l);
+                if(x!=track)
+                    a[j++]=x;
             }
 
             for (int j = 0; j < w; j++) {
-                matrix[i][j] = 0;
-                for (int k = 0; k < max; k++) {
+                matrix[i][j]=0;
+                for(int k=0; k<max; k++)
+                {
                     if (j == a[k])
                         matrix[i][j] = 1;
                 }
             }
-            int j = 0;
-            int start = track;
-            boolean b = true;
-            while (b && track - j >= 0) {
-                b = matrix[i][track - j] == 0;
+            int j=0;
+            int start=track;
+            boolean b=true;
+            while(b && start>=0)
+            {
+                b=matrix[i][start]==0;
+                if(!b)
+                    start++;
+                start--;
+            }
+            start++;
+            j=track-start;
+            b=true;
+            while(b && start+j<matrix[i].length)
+            {
+                System.out.println(i+" : "+j+" : "+(start+j)+ " : " + track);
+                b=matrix[i][start+j]==0;
+                if(!b)
+                    j--;
                 j++;
             }
-            start = track - --j;
-            b = true;
-            while (b && start + j < matrix[i].length) {
-                b = matrix[i][start + j] == 0;
-                j++;
-            }
-            track = start + (int) (Math.random() * j);
+            track=start+(int)(Math.random()*j);
         }
         return matrix;
     }
