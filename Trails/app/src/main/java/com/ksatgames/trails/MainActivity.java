c88cb6faa62eacafbf2ce03c2f95585fa4018f91
Sam Thomas <sgpthomas@gmail.com>
2016-10-02 22:01:02 -0400

    Fullscreen

--- Trails/app/src/main/java/com/ksatgames/trails/MainActivity.java
+++ Trails/app/src/main/java/com/ksatgames/trails/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ksatgames.trails;
 
-import android.os.Bundle;
-
 import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import come.ksatgame.trails.gameEngine.GameView;
 
@@ -17,6 +20,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
         // Initialize gameView and set it as the view
         gameView = new GameView(this);
         setContentView(gameView);
@@ -26,6 +35,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        int uiOptions;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            uiOptions =
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        } else {
+            uiOptions =
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(uiOptions);
 
         // Tell the gameView resume method to execute
         gameView.resume();
